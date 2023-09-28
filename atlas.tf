7fedbd0fd5d26a7d3df9192aa7c986e2e77dc4da
Scott Sickles <scottsickles@gmail.com>
2023-09-28 14:36:46 -0400

    read the atlas api key values from secrets manager

--- atlas.tf
+++ atlas.tf
@@ -1,4 +1,4 @@
 provider "mongodbatlas" {
-  public_key  = local.atlas_public_key
-  private_key = local.atlas_private_key
+  public_key  = data.aws_secretsmanager_secret_version.atlas_public_key.secret_string
+  private_key = data.aws_secretsmanager_secret_version.atlas_private_key.secret_string
 }
--- nullstone.tf
+++ nullstone.tf
@@ -19,17 +19,22 @@ data "ns_connection" "mongo" {
 }
 
 locals {
-  // TODO: make the atlas variables secrets and share the secret id
-  atlas_project_id     = data.ns_connection.mongo.outputs.atlas_project_id
-  atlas_public_key     = data.ns_connection.mongo.outputs.atlas_public_key
-  atlas_private_key    = data.ns_connection.mongo.outputs.atlas_private_key
-  db_protocol          = data.ns_connection.mongo.outputs.db_protocol
-  db_port              = data.ns_connection.mongo.outputs.db_port
-  db_endpoint          = data.ns_connection.mongo.outputs.db_endpoint
-  db_security_group_id = data.ns_connection.mongo.outputs.db_security_group_id
-  db_admin_secret_id   = data.ns_connection.mongo.outputs.db_admin_secret_id
+  atlas_project_id            = data.ns_connection.mongo.outputs.atlas_project_id
+  atlas_public_key_secret_id  = data.ns_connection.mongo.outputs.atlas_public_key_secret_id
+  atlas_private_key_secret_id = data.ns_connection.mongo.outputs.atlas_private_key_secret_id
+  db_protocol                 = data.ns_connection.mongo.outputs.db_protocol
+  db_port                     = data.ns_connection.mongo.outputs.db_port
+  db_endpoint                 = data.ns_connection.mongo.outputs.db_endpoint
+  db_security_group_id        = data.ns_connection.mongo.outputs.db_security_group_id
+  db_admin_secret_id          = data.ns_connection.mongo.outputs.db_admin_secret_id
 }
 
+data "aws_secretsmanager_secret_version" "atlas_public_key" {
+  secret_id = local.atlas_public_key_secret_id
+}
+data "aws_secretsmanager_secret_version" "atlas_private_key" {
+  secret_id = local.atlas_private_key_secret_id
+}
 data "aws_secretsmanager_secret_version" "admin" {
   secret_id = local.db_admin_secret_id
 }
